681d633653142a8aac5180012487f4c50c4dfd07
Platinumstudiosrexx <156248997+Platinumstudiosrexx@users.noreply.github.com>
2024-01-11 00:30:01 +0000

    Add files via upload

---
+++ Copy of form handler.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = $_POST['name'];
+$vistor_email = $_POST ['email'];
+$subject = $_POST ['subject'];
+$message = $_POST ['message'];
+
+$email_from = 'isaacadudarko@gmail.com';
+
+$emial_subject = 'New form Submission';
+
+$email_body = "User Name: $name.\n".
+                "User Email: $vistor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $Message.\n";
+                
+$to = 'jajilinstitute@gmail.com';
+
+$headers = "From: $email_form\r\n";
+
+$headers .="Reply-To: $visitor-email \r\n";   
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("location: cantact.html");
+header("blog-content: blo.html");
+
+  ?>
